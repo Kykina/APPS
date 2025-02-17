a086f24b19a2c2f1ad09a79cd3a3da3617ef3b99
Kateřina Bambušková <katerina.bambuskova@seznam.cz>
2025-02-17 15:29:04 +0100

    Pouziti globalnich proměnných

--- cv1/asm-module.asm
+++ cv1/asm-module.asm
@@ -10,11 +10,22 @@
 ;  datovými typy v jazyce C. Implementuje kopírování int, long, rozšíření
 ;  znaménka u char, nulové rozšíření unsigned char, kopírování stringů
 ;  a kopírování prvků pole pomocí instrukcí MOV, MOVSX a MOVZX.
+;  Pomocí globálních proměnných.
 ;
 ;***************************************************************************
 
 section .data
-    extern global_value  ; Globální proměnná v C
+    extern g_value
+    extern g_int
+    extern g_char
+    extern g_uchar
+    extern g_uint
+    extern g_long_value
+    extern g_long
+    extern g_source
+    extern g_destination
+    extern g_src
+    extern g_dest
 
 section .text
     global copy_int
@@ -26,44 +37,47 @@ section .text
 
 ; Kopírování hodnoty typu int
 copy_int:
-    mov eax, edi
+    mov eax, [g_value]
+    mov [g_int], eax
     ret
 
 ; Sign-extend: rozšíření znaménka pro hodnotu typu char
 sign_extend_char:
-    movsx eax, dil
+    movsx eax, byte [g_char]
+    mov [g_int], eax
     ret
 
 ; Zero-extend: nulové rozšíření pro hodnotu typu unsigned char
 zero_extend_uchar:
-    movzx eax, dil
+    movzx eax, byte [g_uchar]
+    mov [g_uint], eax
     ret
 
 ; Kopírování hodnoty typu long
 copy_long:
-    mov rax, rdi
+    mov rax, [g_long_value]
+    mov [g_long], rax
     ret
 
 ; Kopírování stringu (prvních 5 znaků)
 copy_string:
-    mov al, [rdi]
-    mov [rsi], al
-    mov al, [rdi+1]
-    mov [rsi+1], al
-    mov al, [rdi+2]
-    mov [rsi+2], al
-    mov al, [rdi+3]
-    mov [rsi+3], al
-    mov al, [rdi+4]
-    mov [rsi+4], al
-    mov byte [rsi+5], 0
+    mov al, [g_source]
+    mov [g_destination], al
+    mov al, [g_source+1]
+    mov [g_destination+1], al
+    mov al, [g_source+2]
+    mov [g_destination+2], al
+    mov al, [g_source+3]
+    mov [g_destination+3], al
+    mov al, [g_source+4]
+    mov [g_destination+4], al
     ret
 
 ; Kopírování prvních dvou intů z pole
 copy_two_ints:
-    mov eax, [rdi]
-    mov [rsi], eax
-    mov eax, [rdi+4]
-    mov [rsi+4], eax
+    mov eax, [g_src]
+    mov [g_dest], eax
+    mov eax, [g_src+4]
+    mov [g_dest+4], eax
     ret
 
--- cv1/c-main.c
+++ cv1/c-main.c
@@ -9,6 +9,7 @@
 //  Tento soubor obsahuje hlavní funkci, která volá assemblerové procedury
 //  pro práci s různými datovými typy v jazyce C. Ukazuje operace s int, char,
 //  unsigned char, long a stringy pomocí MOV instrukcí.
+//  S použitím globálních proměnných.
 //
 //***************************************************************************
 
@@ -16,48 +17,62 @@
 #include <stdint.h>
 
 // Deklarace funkcí z Assembleru
-extern int copy_int(int a);
-extern int sign_extend_char(char a);
-extern unsigned int zero_extend_uchar(unsigned char a);
-extern long copy_long(long a);
-extern void copy_string(char *src, char *dest);
-extern void copy_two_ints(int *src, int *dest);
+void copy_int();
+void sign_extend_char();
+void zero_extend_uchar();
+void copy_long();
+void copy_string();
+void copy_two_ints();
 
-// Globální proměnná
-int global_value = 10;
+int g_value;
+int g_int;
+char g_char;
+unsigned char g_uchar;
+unsigned int g_uint;
+long g_long_value;
+long g_long;
+char g_source[];
+char g_destination[];
+int g_src[2];
+int g_dest[2];
+char g_source[] = "Hello, ASM!";
+char g_destination[10] = {0};
+int g_src[2] = {7, 42}; 
+int g_dest[2] = {0, 0};   
+
 
 int main() {
     // Kopírování hodnoty typu int
-    int a = 12345;
-    int result = copy_int(a);
-    printf("MOV (kopie int): %d\n", result);
+    g_value = 12345;
+    g_int = 0;
+    copy_int();
+    printf("MOV (kopie int): %d\n", g_int);
 
     // Sign-extend: rozšíření znaménka pro hodnotu typu char
-    char small_signed = -42;
-    int sign_result = sign_extend_char(small_signed);
-    printf("MOVSX (sign-extend -42): %d\n", sign_result);
+    g_char = -42;
+    g_int = 0;
+    sign_extend_char();
+    printf("MOVSX (sign-extend -42): %d\n", g_int);
 
     // Zero-extend: nulové rozšíření pro hodnotu typu unsigned char
-    unsigned char small_unsigned = 200;
-    unsigned int zero_result = zero_extend_uchar(small_unsigned);
-    printf("MOVZX (zero-extend 200): %u\n", zero_result);
+    g_uchar = 200;
+    g_uint = 0;
+    zero_extend_uchar();
+    printf("MOVZX (zero-extend 200): %u\n", g_uint);
 
     // Kopírování hodnoty typu long
-    long big_number = 1234567890123;
-    long long_result = copy_long(big_number);
-    printf("MOV (kopie long): %ld\n", long_result);
+    g_long_value = 1234567890123;
+    g_long = 0;
+    copy_long();
+    printf("MOV (kopie long): %ld\n", g_long);
 
     // Kopírování stringu
-    char source[] = "Hello, ASM!";
-    char destination[10] = {0};
-    copy_string(source, destination);
-    printf("MOV (kopírování stringu): %s\n", destination);
+    copy_string();
+    printf("MOV (kopírování stringu): %s\n",g_destination);
 
     // Kopírování prvních dvou intů z pole
-    int src[2] = {7, 42};
-    int dest[2] = {0, 0};
-    copy_two_ints(src, dest);
-    printf("MOV (kopírování pole): %d %d\n", dest[0], dest[1]);
+    copy_two_ints();
+    printf("MOV (kopírování pole): %d %d\n",g_dest[0], g_dest[1]);
 
     return 0;
 }
